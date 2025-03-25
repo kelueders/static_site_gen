ca5f533849bd903d3e0faa2a2162277649435741
kelueders <kelueders@gmail.com>
2025-03-24 20:37:16 -0500

    fix markdown_to_blocks()

--- src/blocks_funcs.py
+++ src/blocks_funcs.py
@@ -1,5 +1,33 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = 'paragraph'
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    UNORDERED_LIST = 'unordered_list'
+    ORDERED_LIST = 'ordered_list'
+
 def markdown_to_blocks(markdown):
-    return markdown.split("\n\n")
+    '''
+    Takes a raw Markdown string (representing a full document) as input.
+    Returns a list of "block" strings.
+    '''
+    # Replace two new lines with any number of blank spaces between them with just '\n\n' next to eachother
+    markdown = re.sub(r'\n\s*\n', '\n\n', markdown)
+
+    # Split the text wherever there are two new lines
+    blocks = markdown.split("\n\n")
+
+    # Change all single new lines with any number of blank spaces after with just '\n'
+    fixed_blocks = []
+    for block in blocks:
+        block = re.sub(r'\n\s*', '\n', block)
+        fixed_blocks.append(block.strip())
+        
+    return fixed_blocks
+    
 
 def block_to_block_type(block):
-    
+    pass
--- src/test_blocks_funcs.py
+++ src/test_blocks_funcs.py
@@ -3,12 +3,77 @@ import unittest
 from blocks_funcs import markdown_to_blocks
 
 class TestMarkdowntoBlocks(unittest.TestCase):
-    def test_three_blocks(self):
-        doc = "# This is a heading\n\nThis is a paragraph of text.\n\n* This is the first list item in a list block. \n * This is the second list item."
+    # def test_three_blocks(self):
+    #     doc = '''# This is a heading. 
+        
+    #     This is a paragraph of text.
+
+    #     * This is the first list item in a list block. 
+    #     * This is the second list item.'''
+    #     actual = markdown_to_blocks(doc)
+    #     expected = [
+    #         "# This is a heading",
+    #         "This is a paragraph of text.",
+    #         "* This is the first list item in a list block. \n * This is the second list item."
+    #     ]
+    #     self.assertEqual(expected, actual)
+
+    def test_white_space(self):
+        doc = "   This is a block of text   "
+        actual = markdown_to_blocks(doc)
+        expected = ["This is a block of text"]
+        self.assertEqual(expected, actual)
+
+    def test_double_linebreak(self):
+        doc = '''Or so they say
+        
+        What is going on?    '''
         actual = markdown_to_blocks(doc)
         expected = [
-            "# This is a heading",
-            "This is a paragraph of text.",
-            "* This is the first list item in a list block. \n * This is the second list item."
+            "Or so they say",
+            "What is going on?"
         ]
         self.assertEqual(expected, actual)
+
+    def test_whitespace_blank(self):
+        doc = '''This is a block of TEXT A   
+
+        This is the next line after a blank line      '''
+        actual = markdown_to_blocks(doc)
+        expected = [
+            "This is a block of TEXT A",
+            "This is the next line after a blank line"
+        ]
+        self.assertEqual(expected, actual)
+
+    def test_multiblanks(self):
+        doc = '''This is a block of text B
+
+
+        This is the next line after a blank line      '''
+        actual = markdown_to_blocks(doc)
+        expected = [
+            "This is a block of text B",
+            "This is the next line after a blank line"
+        ]
+        self.assertEqual(expected, actual)
+
+    def test_markdown_to_blocks(self):
+        md = """
+        This is **bolded** paragraph
+                
+        This is another paragraph with _italic_ text and `code` here
+        This is the same paragraph on a new line
+                
+        - This is a list
+        - with items
+        """
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
